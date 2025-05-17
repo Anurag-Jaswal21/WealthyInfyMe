7b3314d5f2faba11cbaf60b89ee87d86a02fc3c4
Anurag-Jaswal21 <anuragjaswal9211@gmail.com>
2025-05-17 12:39:39 +0530

    Update forms.py

--- Wealthyfyme/forms.py
+++ Wealthyfyme/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length,Regexp
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -9,9 +9,9 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50),Regexp(r'^[A-Za-z ]+$', message="Name must contain only letters and spaces")])
+    email = StringField('Email', validators=[DataRequired(), Email(message="Invalid email format"),Regexp(r'^\S+@\S+\.\S+$', message="Invalid email format")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6),Regexp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$',message="Password must include uppercase, lowercase, digit, and special character")])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
     accept_terms = BooleanField('I agree to the Terms & Conditions', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Register')    
